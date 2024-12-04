7ef7b2f40d896b87a1f58941eeec574d14c7bce1
Diogo Xavier <diogodsxavier@gmail.com>
2024-12-04 15:10:00 -0300

    Ajustando componentes

---
+++ src/components/InitialValue.jsx
@@ -0,0 +1,15 @@
+function InitialValue() {
+    return (
+        <div>
+            <label htmlFor="number" className="block text-sm/6 font-medium text-gray-900">Valor inicial</label>
+            <div className="mt-2">
+                <input id="InitialValue" 
+                type="number" required 
+                placeholder='R$00,00' 
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm/6" />
+            </div>
+        </div>
+    )
+}
+
+export default InitialValue;
---
+++ src/components/Tittle.jsx
@@ -0,0 +1,9 @@
+function Tittle() {
+    return (
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+            <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">Simulador de juros compostos</h2>
+        </div>
+    )
+}
+
+export default Tittle;
